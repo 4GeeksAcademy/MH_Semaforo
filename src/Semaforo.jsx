38ccaa64ddf5c4a718a2551f2300e6a71c2ab9a8
mhavliczek <mhavliczek@gmail.com>
2024-09-16 19:31:47 -0300

    cambios

--- src/Semaforo.jsx
+++ src/Semaforo.jsx
@@ -3,29 +3,41 @@ import './Semaforo.css';
 
 export const Semaforo = () => {
   const [color, setColor] = useState("red"); // Inicializamos el estado con el color rojo
-  const [sequence, setSequence] = useState(["red", "yellow", "green"]); // Creamos un array con la secuencia de colores
+  const [sequence, setSequence] = useState(["green","yellow","red"]); // Creamos un array con la secuencia de colores
   const [currentIndex, setCurrentIndex] = useState(0); // Creamos un índice para la secuencia de colores
 
   const handleLightClick = () => {
-    const nextIndex = (currentIndex + 1) % sequence.length; // Calculamos el próximo índice en la secuencia
+    if (color=="red"){
+      setColor("green");
+    }else if(color=="yellow"){
+      setColor("red");
+    }else if(color=="green"){
+      setColor("yellow");
+    }
+      
+    setColor(color_selecionado);
+    /*const nextIndex = (currentIndex + 1) % sequence.length; // Calculamos el próximo índice en la secuencia
     const nextColor = sequence[nextIndex]; // Obtenemos el próximo color en la secuencia
     setColor(nextColor); // Actualizamos el estado con el nuevo color
-    setCurrentIndex(nextIndex); // Actualizamos el índice en la secuencia
+    setCurrentIndex(nextIndex); // Actualizamos el índice en la secuencia*/
   };
 
   return (
     <div id="semaforo">
       <div
         className={`luz ${color === "red" ? "activa" : "inactiva"} roja`}
-        onClick={() => handleLightClick()}
+        data-color="red"
+        onClick={(e) => setColor(e.target.getAttribute("data-color"))}
       ></div>
       <div
         className={`luz ${color === "yellow" ? "activa" : "inactiva"} amarilla`}
-        onClick={() => handleLightClick()}
+        data-color="yellow"
+        onClick={(e) => setColor(e.target.getAttribute("data-color"))}
       ></div>
       <div
         className={`luz ${color === "green" ? "activa" : "inactiva"} verde`}
-        onClick={() => handleLightClick()}
+        data-color="green"
+        onClick={(e) => setColor(e.target.getAttribute("data-color"))}
       ></div>
       <div></div>
       <button onClick={handleLightClick}>Cambiar color</button>
